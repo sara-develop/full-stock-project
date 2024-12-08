b00c910639ea39bdc95a05688a5057d43c29eea4
sara-develop <sarastepen1@gmail.com>
2024-12-08 22:18:40 +0200

    second commit

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 1100
 app.get("/", (req, res) => {
     res.send("<h1>Welcome to the homepage:)</h1>")
 })
+app.get("/ab", (req, res) => {
+    res.send("<h1>Welcome to the ab page:)</h1>")
+})
 
 app.listen(PORT, () => {
     console.log(`the server is runing on port ${PORT}`)
