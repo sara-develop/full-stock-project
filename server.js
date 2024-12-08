fa398ea0c8a436cc5e3902d4790aaeca6058103e
sara-develop <sarastepen1@gmail.com>
2024-12-08 22:10:06 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT || 1100
+
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to the homepage:)</h1>")
+})
+
+app.listen(PORT, () => {
+    console.log(`the server is runing on port ${PORT}`)
+})
